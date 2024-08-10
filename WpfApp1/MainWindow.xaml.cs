5d3dc2701c3a1a5c67bb8fdca0f2a05cfb06cab0
药吃猫 <1121747528@qq.com>
2024-08-10 17:57:12 +0800

    更新一个小demo绘图.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using ScottPlot.AxisPanels;
+using ScottPlot.DataSources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.cs;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private TimerA timerA;
+        readonly ScottPlot.Plottables.DataLogger Logger1;
+        private bool stoped;
+        // ScottPlot.DataGenerators.RandomWalker Walker1 = new(0, multiplier: 0.01);
+        public MainWindow()
+        {
+           
+
+            InitializeComponent();
+            stoped=false;
+            timerA = new TimerA();
+            this.DataContext = timerA; //设置之后绑定才会有效
+
+            //double[] dataX = { 1, 2, 3, 4, 5 };
+            //double[] dataY = { 1, 4, 9, 16, 25 };
+            //WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+            //WpfPlot1.Refresh();
+
+
+            timerA.PropertyChanged += timerAPropertyChanged;
+
+            WpfPlot1.Interaction.Disable();
+            Logger1 = WpfPlot1.Plot.Add.DataLogger();
+
+            RightAxis axis1 = (RightAxis)WpfPlot1.Plot.Axes.Right;
+            Logger1.Axes.YAxis = axis1;
+            axis1.Color(Logger1.Color);
+
+
+        }
+        void timerAPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (!stoped&&e.PropertyName == "Points")
+            {
+                Logger1.Add(timerA.Points.X, timerA.Points.Y);
+                WpfPlot1.Refresh();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            stoped=true;
+            timerA.PropertyChanged -= timerAPropertyChanged;//取消事件触发
+
+            DataLoggerSource a = new DataLoggerSource();
+            a = Logger1.Data;
+            List<Double> B = new List<Double>();
+            List<Double> C = new List<Double>();
+            foreach (var item in a.Coordinates) {
+                B.Add(item.X);
+                C.Add(item.Y);
+            }
+            WpfPlot1.Plot.Clear();
+            WpfPlot1.Interaction.Enable();
+            WpfPlot1.Plot.Add.Scatter(B, C);
+            WpfPlot1.Refresh();
+        }
+    }
+}
---
+++ WpfApp1/cs/timer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfApp1.cs
+{
+    public class TimerA: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string m_dateTime;
+        private int X;
+        private Point m_Point ;
+        public string DateTimea { get { return m_dateTime; } set { m_dateTime = value; OnPropertyChanged(nameof(DateTimea)); } }
+        public Point Points { get { return m_Point; }set { m_Point = value; OnPropertyChanged2(nameof(Points)); } }
+            private DispatcherTimer _timer;
+        public TimerA()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);//更新频率设为每秒
+            _timer.Tick += Time_Tick;
+            _timer.Start();
+            X = 0;
+        }
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            updateTime();
+            updatepoint();
+        }
+        private void updateTime()
+        {
+            DateTimea = DateTime.Now.ToString("HH:mm:ss");
+
+        }
+        private void updatepoint()
+        {
+            Random ran = new Random();
+            int m = ran.Next(10000);
+            Points = new Point(X, m);
+            X++;
+
+        }
+        private void OnPropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+        private void OnPropertyChanged2(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+    }
+}
